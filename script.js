92acb177c9d7cddd3763bc4d8488e3f7c59b50d2
BryanOliv <130477352+BryanOliv@users.noreply.github.com>
2024-10-30 12:39:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let produto = {
+    nome: 'camiseta regata branca',
+    preco: 150,
+    emEstoque: true
+    };
+    
+let inventario = [
+    { nome: 'regata branca', preco: 150, emEstoque: true },
+    { nome: 'regata preta', preco: 150, emEstoque: false },
+    { nome: 'regata cinza', preco: 150, emEstoque: true },
+    { nome: 'camiseta social preta', preco: 200, emEstoque: true },
+    { nome: 'camiseta social branca', preco: 200, emEstoque: false },
+    { nome: 'calsa cargo preta', preco: 301, emEstoque: true },
+    { nome: 'calsa cargo verde escuro', preco: 301, emEstoque: true },
+    { nome: 'calsa cargo camuflada', preco: 301, emEstoque: false },
+    { nome: 'moletom com capuz preto', preco: 299, emEstoque: true },
+    { nome: 'moletom com capuz branco', preco: 299, emEstoque: true },
+    { nome: 'moletom com capuz marrom', preco: 299, emEstoque: false },
+    { nome: 'moletom sem capuz preto', preco: 299, emEstoque: true },
+    { nome: 'moletom sem capuz branco', preco: 299, emEstoque: true },
+    { nome: 'moletom sem capuz marrom', preco: 299, emEstoque: false },
+    { nome: 'shorts tactel preto', preco: 60, emEstoque: true },
+    { nome: 'shorts tactel branco', preco: 60, emEstoque: true },
+    { nome: 'par de meias pretas', preco: 15, emEstoque: false },
+    { nome: 'par de meias cinzas', preco: 15, emEstoque: true },
+    { nome: 'par de meias brancas', preco: 15, emEstoque: true },
+    { nome: 'par de meias marrons', preco: 15, emEstoque: false }
+    ];
